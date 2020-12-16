2000e62f95b74abade162fa7668a52df0cc3b3be
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-16 09:12:08 +0100

    first steps

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 // Import the React and React-DOM librairies
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-const App = () => <div>Hi there!</div>;
+const App = () => {
+  const buttonText = 'Click Me!';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }} type="submit">
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
